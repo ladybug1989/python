ca760661df9b119c5a7e1ab8f87a2e2bfeb66b5c
Berlin Patience <starsberlin@gmail.com>
2022-07-03 21:34:28 -0400

    Object Oriented Programming

---
+++ core.py
@@ -0,0 +1,16 @@
+from product import Item
+
+designer_item = Item("Chevy", "Corvette", 2021, "blue", "fall season")
+print(designer_item.designer)
+print(designer_item.model_number)
+print(designer_item.year)
+print(designer_item.color)
+print(designer_item.fashion_season)
+
+designer_item.shoes()
+designer_item.clothing()
+
+
+
+
+
---
+++ product.py
@@ -0,0 +1,14 @@
+class Item:
+
+    def __init__(self, designer, model_number, year, color, fashion_season):
+        self.designer = designer
+        self.model_number = model_number
+        self.year = year
+        self.color = color
+        self.fashion_season = fashion_season
+
+    def shoes(self):
+        print("The shoes for the year ")
+
+    def clothing(self):
+        print("This clothing for the year")
